2afa43e563290305e5568878ca67f6a7209fad55
ZhekoGinev <jginev@gmail.com>
2022-04-30 17:37:47 +0300

    add models for Article and the ArticleAdmin

--- blog/apps/mainpage/admin.py
+++ blog/apps/mainpage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on')
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ['title', 'content']
+
+
+admin.site.register(Article, ArticleAdmin)
--- blog/apps/mainpage/views.py
+++ blog/apps/mainpage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Article
 
 # Create your views here.
 def index(request):
