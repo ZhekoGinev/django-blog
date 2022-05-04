8ce55e3ba1835350e0142dde27068a5da8cb28f2
ZhekoGinev <jginev@gmail.com>
2022-05-04 16:03:20 +0300

    change structure, add templates and links for home, articles, about and contact

--- blog/apps/mainpage/urls.py
+++ blog/apps/mainpage/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="homepage"),
+    path("", views.index, name="homepage"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.about, name="contact"),
+    path("articles/", views.about, name="articles"),
 ]
--- blog/apps/mainpage/views.py
+++ blog/apps/mainpage/views.py
@@ -3,4 +3,16 @@ from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainpage/index.html')
+    return render(request, "mainpage/index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def articles(request):
+    return render(request, "articles.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
